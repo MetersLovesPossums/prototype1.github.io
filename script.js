e96c7484c3dad63c4824cc1196116ef6c46df126
Miles <milesmoore492@gmail.com>
2025-05-18 20:13:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+button2.addEventListener('click', function() {
+	button2.textContent = "I didnt finish this, sorry"
+});
+button3.addEventListener('click', function() {
+	button3.textContent = "I didnt finish this either"
+});
